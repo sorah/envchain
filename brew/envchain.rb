d6d5d2b7c93db9ba887a3acac3ccf48ec5933ca5
Shota Fukumori (sora_h) <sorah@cookpad.com>
2015-05-12 17:57:06 +0900

    update formula

--- brew/envchain.rb
+++ brew/envchain.rb
@@ -3,8 +3,9 @@ require "formula"
 class Envchain < Formula
   homepage "https://github.com/sorah/envchain"
   head "https://github.com/sorah/envchain.git"
-  url "https://github.com/sorah/envchain/archive/v0.2.0.tar.gz"
-  sha1 "7d100ddbd118475ef094b25da7c361f921764e89"
+  url "https://github.com/sorah/envchain/archive/v0.3.0.tar.gz"
+  sha1 "17981bebd628575d8fd61ae3ff29612c04fdc828"
+  version '0.3.0'
 
   def install
     system "make", "DESTDIR=#{prefix}", "install"
